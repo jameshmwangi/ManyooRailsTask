ce5a01ebe59341c167bf1b989c73bc007b6d4f59
James Mwangi K <jameshmwangiy@gmail.com>
2026-02-09 13:39:13 +0300

    fix: flash message redirect

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,7 +18,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to tasks_path, notice: 'Task was successfully created.'
+      redirect_to tasks_path, notice: 'Task was successfully created.', status: :see_other
     else
       render :new, status: :unprocessable_entity
     end
