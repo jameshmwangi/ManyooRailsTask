926046ca0a8ab77f47f5f9592f2acca660e170be
James Mwangi K <jameshmwangiy@gmail.com>
2026-02-10 15:57:12 +0300

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,10 @@
 # exit on error
 set -o errexit
 
+export NODE_OPTIONS=--openssl-legacy-provider
+
+bundle config set --local frozen false
+bundle config unset deployment
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
