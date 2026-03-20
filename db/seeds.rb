e082ddd1acdb17c8375bbaf7e82c515f3ff8d3c2
James Mwangi K <jameshmwangiy@gmail.com>
2026-03-21 00:12:34 +0300

    removed duplicate guard on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,5 @@
-if Task.count.zero?
-  50.times do |i|
-    Task.create!(
-      title: "Task #{i + 1}",
-      content: "This is the content for task #{i + 1}."
-    )
-  end
+50.times do |i|
+  Task.create!(
+    title: "Task #{i + 1}",
+    content: "This is the content for task #{i + 1}." )
 end
